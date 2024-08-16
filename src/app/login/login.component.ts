4d92a38c53f57cb7285a9e29e8c5fd91282867ce
Isaac-McClure <isaac.mcclure.3@gmail.com>
2024-08-16 13:09:04 +1000

    Got initial 'login' to work.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SPACE_TRADERS_API_KEY_COOKIE_NAME } from '../constants';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  standalone: true,  
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  apiKey: string = '';
+  remeberOnThisDevice: boolean = false;
 
+  constructor(private router: Router) {}
+
+  login(){
+    console.log('logging in');
+    console.log('api key: ' + this.apiKey);
+    console.log('remeberOnThisDevice: ' + this.remeberOnThisDevice);
+    this.saveApiKey();
+    this.redirectToHome();
+  }
+
+  saveApiKey() {
+    if(this.remeberOnThisDevice){
+      localStorage.setItem(SPACE_TRADERS_API_KEY_COOKIE_NAME, this.apiKey);
+    }
+    else {
+      sessionStorage.setItem(SPACE_TRADERS_API_KEY_COOKIE_NAME, this.apiKey);
+    }
+  }
+
+  redirectToHome(){
+    this.router.navigate(['/home']);
+  }
 }
